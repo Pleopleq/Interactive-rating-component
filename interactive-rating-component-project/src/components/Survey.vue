bffad1f47deaae6cbf8dd6fd50e32cf355d513bc
Felix Anducho <felix.anducho74@gmail.com>
2022-06-29 20:08:43 -0500

    Populate the body of the survey component

--- interactive-rating-component-project/src/components/Survey.vue
+++ interactive-rating-component-project/src/components/Survey.vue
@@ -1,6 +1,21 @@
 <template>
   <section class="survey__container">
-    <survey-content></survey-content>
+    <survey-content>
+      <template v-slot:title>
+        <div>
+          <div class="starIcon">
+            <img src="../assets/images/icon-star.svg" alt="" srcset="" />
+          </div>
+          <h1>How did we do?</h1>
+        </div>
+      </template>
+      <template v-slot:body>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+      </template>
+    </survey-content>
   </section>
 </template>
 
--- interactive-rating-component-project/src/components/SurveyContent.vue
+++ interactive-rating-component-project/src/components/SurveyContent.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="surveyContent__container">
+  <section class="surveyContent__container">
     <div class="surveyContent__title">
       <slot name="title"></slot>
     </div>
     <div class="surveyContent__body">
       <slot name="body"></slot>
     </div>
-  </div>
+    <div class="surveyContent__rating">
+      <form>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>Submit</button>
+      </form>
+    </div>
+  </section>
 </template>
 
 <script>
